192a559e3efe70c18220391a5aa9df6269c189be
Emerson Henning <emerson@henning.com.br>
2022-03-14 15:24:32 -0300

    change settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config, Csv
 from dj_database_url import parse as dburl
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
