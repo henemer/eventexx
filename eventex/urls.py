d182ea5c53befe135b9ce8788da77b4cdc8666ad
Emerson Henning <emerson@henning.com.br>
2022-03-30 14:33:01 -0300

    Thank you page

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from eventex.subscriptions.views import subscribe
+from eventex.subscriptions.views import subscribe, detail
 
 import eventex.core.views
 
 urlpatterns = [
     path('', eventex.core.views.home),
     path('inscricao/', subscribe),
+    path('inscricao/<int:pk>/', detail),
     path('admin/', admin.site.urls),
     
 ]
