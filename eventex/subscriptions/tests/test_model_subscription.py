d5523e8a5e6d03f0e40516cc9676957e6d4363a7
Emerson Henning <emerson@henning.com.br>
2022-04-04 15:32:38 -0300

    Adiciona campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -23,4 +23,8 @@ class SubscriptionModelTest(TestCase):
     def test_str(self):
         self.assertEqual('Emerson Henning', str(self.obj))
 
+    def test_paid_default_to_False(self):
+        """By default  paid must be False"""
+        self.assertEqual(False, self.obj.paid)
+
     
