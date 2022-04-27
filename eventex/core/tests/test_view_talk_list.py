fb5ae76f1350d1fc4a974ca5cb600b9c7c8bd259
Emerson Henning <emerson@henning.com.br>
2022-04-27 10:45:28 -0300

    Course como MTI

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -47,7 +47,7 @@ class TalklistGet(TestCase):
 
 
     def test_context(self):
-        variables = ['morning_talks', 'afternoon_talks', 'courses']
+        variables = ['morning_talks', 'afternoon_talks']
 
         for key in variables:
             with self.subTest():
